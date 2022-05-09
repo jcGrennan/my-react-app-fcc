a5c00751006084d181963a02552b1646c3fb4ac8
JC Grennan <jgrennan94@gmail.com>
2022-05-10 00:06:08 +0100

    stuff

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React WOW
+          Learn React
         </a>
       </header>
     </div>
