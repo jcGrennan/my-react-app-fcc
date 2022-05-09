b6011ede63452cf8a08b7c3eac91fcd054da382c
JC Grennan <jgrennan94@gmail.com>
2022-05-10 00:39:25 +0100

    step 6.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import { ReactComponent as Logo } from "./logo.svg"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src="\logo.svg" className="App-logo" alt="logo" /> */}
+        <Logo style = {{height: 200}} />
         <h1>React Posts Sharer</h1>
       </header>
     </div>
